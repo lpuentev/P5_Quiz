2bca2bd34dc0d5efa7b941210bdf7006f737ee7b
lpuentev <36475371+lpuentev@users.noreply.github.com>
2018-05-09 18:41:17 +0200

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+const {models} = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index',{ title: 'P5_Quiz' });
 });
-
-router.get('/credits', function (req, res, next){
-  res.render('credits', { title: 'Credits' })
+router.get('/credits', function(req, res, next) {
+  res.render('credits');
 });
 
 router.get('/quizzes', function(req, res, next) {
-  res.render('quizzes', { title: 'Quizzes' });
+
+    models.quiz.findAll()
+        .then( array => {
+            res.render('quizzes/index', { quizzes: array})
+
+        })
+        .catch( error => {
+            console.log(error.message)
+        });
+
 });
 
 module.exports = router;
