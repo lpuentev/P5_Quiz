63156e7c830d924854bfb235d3a12320937437e4
lpuentev <36475371+lpuentev@users.noreply.github.com>
2018-05-09 18:40:54 +0200

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -1,17 +1,3 @@
-//var express = require('express');
-//var router = express.Router();
-
-///* GET home page. */
-//router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-//});
-
-//router.get('/credits', function (req, res, next){
-  //res.render('credits', { title: 'Credits' })
-//});
-
-//module.exports = router;
-
 
 
 const Sequelize = require('sequelize');
