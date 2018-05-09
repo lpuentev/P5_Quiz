2ef814f16b37302a1c0992ff2712d642b0fe2df4
lpuentev <36475371+lpuentev@users.noreply.github.com>
2018-05-09 19:32:31 +0200

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -1,3 +1,17 @@
+//var express = require('express');
+//var router = express.Router();
+
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+//});
+
+//router.get('/credits', function (req, res, next){
+  //res.render('credits', { title: 'Credits' })
+//});
+
+//module.exports = router;
+
 
 
 const Sequelize = require('sequelize');
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const {models} = require('../models/index');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index',{ title: 'P5_Quiz' });
+  res.render('index', { title: 'Express' });
 });
-router.get('/credits', function(req, res, next) {
-  res.render('credits');
+
+router.get('/credits', function (req, res, next){
+  res.render('credits', { title: 'Credits' })
 });
 
 router.get('/quizzes', function(req, res, next) {
-
-    models.quiz.findAll()
-        .then( array => {
-            res.render('quizzes/index', { quizzes: array})
-
-        })
-        .catch( error => {
-            console.log(error.message)
-        });
-
+  res.render('quizzes', { title: 'Quizzes' });
 });
 
 module.exports = router;
